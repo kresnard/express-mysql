44363bb5e291cddfc22a7680df2cb94810da0300
kresnard <kresnard@gmail.com>
2023-06-12 10:44:17 +0700

    upload file endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 import express from 'express';
 import userRoutes from './routes/users.js'
 import middlewareLog from './middleware/logs.js'
+import upload from './middleware/multer.js';
 
 dotenv.config()
 
@@ -9,11 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(middlewareLog)
-app.use(express.json())
-app.use('/assets', express.static('public/images'))
+app.use(middlewareLog);
+app.use(express.json());
+app.use('/assets', express.static('public/images'));
 
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
+app.post('/upload', upload.single('photo'), (req, res) => {
+    res.json({
+        message: 'Success upload!'
+    })
+})
 
 
 
